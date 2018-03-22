4813c84cc9c1b5b50e918e7373b13d33bb95b817
webdevBeginner <37663902+webdevBeginner@users.noreply.github.com>
2018-03-22 22:58:37 +0800

    tiele
    content

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="hello">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+	$red:#f60;
+	div {
+		color: $red;
+	}
+	
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+	
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+	
+	a {
+		color: #42b983;
+	}
+</style>
